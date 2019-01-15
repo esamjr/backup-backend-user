70f5ba8b832782a5b871f37a8c42d65d4bca91b7
maulidan <maulidan.ksl@gmail.com>
2019-01-15 15:59:35 +0700

    award\models.py

--- award/models.py
+++ award/models.py
@@ -4,7 +4,7 @@ class Award(models.Model):
   id_user = models.IntegerField()
   id_institution = models.IntegerField(null=True,blank=True)
   name_institution = models.CharField(max_length=255,null=True,blank=True)
-  name_institution = models.CharField(max_length=255,null=True,blank=True)
+  award = models.CharField(max_length=255,null=True,blank=True)
   email_institution = models.CharField(max_length=255,null=True,blank=True)
   date_received = models.DateField()
   verfied = models.CharField(max_length=3)
