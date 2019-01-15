c62aba26e50eea540e20ce96e3cf822d6de946eb
maulidan <maulidan.ksl@gmail.com>
2019-01-15 17:09:05 +0700

    response certification

--- certification/views.py
+++ certification/views.py
@@ -65,13 +65,13 @@ def get_post_certification(request):
 api_view(['GET', 'POST'])
 
 def get_post_certification_user(request,pk):
-    try:
-        if request.method == 'GET':
+    if request.method == 'GET':
+         try:
             network = certificate.objects.all().filter(id_user=pk)
             serializer = CertificationSerializer(network, many=True)
             return Response(serializer.data)
-    except certificate.DoesNotExist:
-        content = {
-            'status': 'Not Found'
-        }
-        return Response(content, status=status.HTTP_404_NOT_FOUND)
+        except certificate.DoesNotExist:
+            content = {
+                'status': 'Not Found'
+            }
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
