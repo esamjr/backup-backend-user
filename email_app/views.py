c8aef4dfd58cae52ef2fa023aaad7daf02da6d2a
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-17 13:38:25 +0700

    reroute

--- email_app/views.py
+++ email_app/views.py
@@ -19,7 +19,7 @@ def send_email(mail,token):
 
 	send_mail(
 		'Account Activation',
-		'Hi '+respondentEmail.full_name +'\n Thanks so much for joining Mindzzle! \n To finish signing up, you just need to confirm that we got your email right.\n click this button to verify your account \n <button href ="http://127.0.0.1:8000/registrations/api/confirm/"'+token+'">Verify Account</button> \n <a href="http://127.0.0.1:8000/registrations/api/confirm/'+token+'"> Click Here! </a> To verify',
+		'Hi '+respondentEmail.full_name +'\n Thanks so much for joining Mindzzle! \n To finish signing up, you just need to confirm that we got your email right.\n click this button to verify your account \n <button href ="http://www.user.mindzzle.com/registrations/api/confirm/'+token+'">Verify Account</button> \n <a href="http://www.user.mindzzle.com/registrations/api/confirm/'+token+'"> Click Here! </a> To verify',
 		'admin@mindzzle.com',
 		[respondentEmail.email], 
 		fail_silently=False
