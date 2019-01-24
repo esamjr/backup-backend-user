0e1198b5b7442cdf17ef0dfd0100b3c5deaf80cb
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-25 00:19:30 +0700

    logo_path type string

--- business_account/models.py
+++ business_account/models.py
@@ -11,7 +11,7 @@ class Business(models.Model):
     id_city = models.IntegerField(null=True, blank=True)
     id_business_type = models.IntegerField(null=True, blank=True)
     tax_num = models.IntegerField(null=True, blank=True)
-    logo_path = models.FilePathField(blank = True, null=True)
+    logo_path = models.CharField(max_length=255,blank = True, null=True)
     description = models.CharField(max_length=1000,blank=True, null = True)
     banned_type = models.CharField(max_length=500, blank = True)
     create_at = models.DateTimeField(auto_now_add=True)
