1697c1ca012d6df03eb1f519a77b5211d176080a
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-26 18:42:14 +0700

    registrations

--- registrations/urls.py
+++ registrations/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^api/(?P<pk>[0-9]+)$', views.get_delete_update_registrations, name='get_delete_update_registrations'),
+    url(r'^api/findfriends/(?P<pk>[0-9]+)$', views.get_user, name='get_user'),
     url(r'^api/$', views.get_post_registrations, name='get_post_registrations'),
     url(r'^api/login/$', views.get_login, name='get_login'),
     url(r'^api/search/$', views.search, name='search'),
