0572afa3ee26e8bf68d43e9640140020337489d8
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-29 13:25:02 +0700

    joincompany

--- join_company/views.py
+++ join_company/views.py
@@ -4,46 +4,34 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from .models import Joincompany
+from registrations.models import Register
 from .serializers import JoincompanySerializer
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def get_delete_update_joincompany(request, pk):
     try:
-        join_company = Joincompany.objects.get(pk=pk)
-    except Joincompany.DoesNotExist:
+        token = request.META.get('HTTP_AUTHORIZATION')
+        user = Register.objects.get(token=token)
+    except Register.DoesNotExist:
         content = {
-            'status': 'Not Found'
+            'status': 'UNAUTHORIZED'
         }
-        return Response(content, status=status.HTTP_404_NOT_FOUND)
-    
+        return Response(content, status=status.HTTP_401_UNAUTHORIZED)    
     if request.method == 'GET':
         serializer = JoincompanySerializer(join_company)
         return Response(serializer.data)
-
-    elif request.method == 'DELETE':
-        if(request.user == Joincompany):
+    elif request.method == 'DELETE':        
             join_company.delete()
             content = {
-                'status' : 'NO CONTENT'
+                'status' : 'EXTERMINATE...EXTERMINATE...'
             }
-            return Response(content, status=status.HTTP_202_NO_CONTENT)
-        else:
-            content = {
-                'status' : 'UNAUTHORIZED'
-            }
-            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
-    elif request.method == 'PUT':
-        if(request.user == Joincompany):
+            return Response(content, status=status.HTTP_202_NO_CONTENT)        
+    elif request.method == 'PUT':      
             serializer = JoincompanySerializer(Joincompany, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            content = {
-                'status': 'UNAUTHORIZED'
-            }
-            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['GET', 'POST'])
 def get_post_joincompany(request):
