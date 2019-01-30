bbf140aa8cd1153e853723267dcce8092bd2e21a
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-30 15:03:47 +0700

    email views

--- email_app/views.py
+++ email_app/views.py
@@ -12,6 +12,7 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
+from django.conf import settings
 
 
 @csrf_exempt
@@ -34,26 +35,26 @@ def send_forget_email(request,mail,token, name, subjects):
 		response = {'status failed to send email'}
 		return HttpResponse(response)
 
-@csrf_exempt
-def send_email(request, mail, token, name, subjects):
-	respondentEmail = mail
-	sender = 'admin@mindzzle.com'	
-	try:		
-		plaintext = get_template('email.txt')
-		htmly     = get_template('emailtemplate.html')
-		d = ({'username': name,'token': 'http://dev-user.mindzzle.com/register/confirmation?token='+token, 'konten':'To complete your sign up, please verify your email', 'tombol':'Verify Email'})
-		subject, from_email, to = subjects, sender, respondentEmail
-		text_content = plaintext.render(d)
-		html_content = htmly.render(d)
-		msg = EmailMultiAlternatives(subjects, text_content, sender, [mail])
-		msg.attach_alternative(html_content, "text/html")
-		msg.send()
-		email_log(request, respondentEmail,sender,subjects)
-		response = {'status Email Sent'}
-		return HttpResponse(response)
-	except:
-		response = {'status failed to send email'}
-		return HttpResponse(response)
+# @csrf_exempt
+# def send_email(request, mail, token, name, subjects):
+# 	respondentEmail = mail
+# 	sender = 'admin@mindzzle.com'	
+# 	try:		
+# 		plaintext = get_template('email.txt')
+# 		htmly     = get_template('emailtemplate.html')
+# 		d = ({'username': name,'token': 'http://dev-user.mindzzle.com/register/confirmation?token='+token, 'konten':'To complete your sign up, please verify your email', 'tombol':'Verify Email'})
+# 		subject, from_email, to = subjects, sender, respondentEmail
+# 		text_content = plaintext.render(d)
+# 		html_content = htmly.render(d)
+# 		msg = EmailMultiAlternatives(subjects, text_content, sender, [mail])
+# 		msg.attach_alternative(html_content, "text/html")
+# 		msg.send()
+# 		email_log(request, respondentEmail,sender,subjects)
+# 		response = {'status Email Sent'}
+# 		return HttpResponse(response)
+# 	except:
+# 		response = {'status failed to send email'}
+# 		return HttpResponse(response)
 
 
 @csrf_exempt
@@ -115,3 +116,34 @@ def email_get(request):
 
 
 # "http://www.user.mindzzle.com/registrations/api/confirm/'+token+'"
+
+# @csrf_exempt
+@api_view(['POST'])
+# def send_email(request,mail,token, subjects):
+def send_email(request):
+	# respondentEmail = mail
+
+	sender = 'admin@mindzzle.com'
+	subjects = 'Account Activation'
+	respondentEmail = request.data['respondentEmail']
+	name = request.data['sender']
+	# subjects = request.data['subjects']
+	token = 'okasokdianfammcdajsnckm'
+	try:		
+	
+		plaintext = get_template('email.txt')
+		htmly     = get_template('emailtemplate.html')
+
+		d = ({'username': name,'token': 'http://dev-user.mindzzle.com/register/confirmation?token='+token, 'konten':'To complete your sign up, please verify your email', 'tombol':'Verify Email'})
+
+		subject, from_email, to = subjects, sender, respondentEmail
+		text_content = plaintext.render(d)
+		html_content = htmly.render(d)
+		msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+		msg.attach_alternative(html_content, "text/html")
+		msg.send()
+		response = {'success to send email'}
+		return HttpResponse(response)
+	except:
+		response = {'status failed to send email'}
+		return HttpResponse(response)
