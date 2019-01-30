97b5b20dd6e8f6b3e3904a35f60d1d483b6efc41
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-01-30 20:32:21 +0700

    baru lho

--- level/models.py
+++ level/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Level(models.Model):
   id_job_contract = models.IntegerField()
-  id_company = models.IntegerField()
+  id_company = models.IntegerField(null=False)
   level = models.CharField(max_length=255)
   status_parent = models.CharField(max_length=255)
   status_child = models.CharField(max_length=255)
