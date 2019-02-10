012069b716f1176b6f5e344c2c33cbbd6330cbea
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-02-10 17:57:23 +0700

    ocrt user

--- OCR_Reader/views.py
+++ OCR_Reader/views.py
@@ -5,15 +5,18 @@ from urllib.request import urlopen
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework import status
 @csrf_exempt
-def OCRT(request, nomor, url):	
-	link = urlopen(url)
-	im = Image.open(link)
-	text = image_to_string(im, lang = 'ind')
-	if (nomor in text) :
-		return Response({nomor})
-	return Response({'tidak valid'})
+def OCRT(request, nomor, url):
+	if request.method == 'POST':
+		link = urlopen(url)
+		im = Image.open(link)
+		text = image_to_string(im, lang = 'ind')
+		if (nomor in text) :
+			response = {'status' : 'OK'}
+			return Response(response, status = status.HTTP_201_CREATED)
+		response = {'status' : 'NOT OK'}
+		return Response(response, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def OCRTx(request):
@@ -23,5 +26,5 @@ def OCRTx(request):
 	im = Image.open(link)
 	text = image_to_string(im, lang = 'ind')
 	if (nomor in text) :
-		return Response({nomor})
+		return Response({'valid'})
 	return Response({'tidak valid'})
--- user_img/views.py
+++ user_img/views.py
@@ -7,7 +7,11 @@ from rest_framework.parsers import JSONParser
 from .models import User_img
 from OCR_Reader.serializers import UserImgSerializer
 from registrations.models import Register
-from OCR_Reader.views import OCRT
+# from OCR_Reader.views import OCRT
+from PIL import Image
+from pytesseract import image_to_string
+from urllib.request import urlopen
+
 @api_view(['POST'])
 def upload_doc(request):
 	try :
@@ -23,14 +27,20 @@ def upload_doc(request):
 				'url' : url,
 				'nomor' : nomor,
 				'status' : "1"}
-		OCRT(request, nomor, url)
-		serializers = UserImgSerializer(data = payload)
-		if serializers.is_valid():			
-			serializers.save()
-			response = {'status' : 'OK'}
-			return Response(response, status = status.HTTP_201_CREATED)
-		response = {'status' : 'NOT OK'}
-		return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+
+		link = urlopen(url)
+		im = Image.open(link)
+		text = image_to_string(im, lang = 'ind')
+		if (nomor in text) :
+			serializers = UserImgSerializer(data = payload)
+			if serializers.is_valid():			
+				serializers.save()
+				response = {'status' : 'OK'}
+				return Response(response, status = status.HTTP_201_CREATED)
+		else:			
+			response = {'status' : 'NOT OK'}
+			return Response(response, status = status.HTTP_400_BAD_REQUEST)
+
 	except Register.DoesNotExist:
 		response = {'status' : 'Register Does Not Exist'}
 		return Response(response, status = status.HTTP_401_UNAUTHORIZED)
