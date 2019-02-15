c2ae82027341f7907a5bc7f1d52122f8d3baf49f
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-02-15 15:51:39 +0700

    model review scheduler + deadline

--- review_scheduler/models.py
+++ review_scheduler/models.py
@@ -4,5 +4,6 @@ class review_scheduler(models.Model):
 	id_company = models.IntegerField()
 	yearly = models.CharField(max_length = 5)
 	type_scheduller = models.CharField(max_length=10)
+	deadline = models.DateField()
 	created_at = models.DateTimeField(auto_now_add = True)
 	update_at = models.DateTimeField(auto_now = True)
