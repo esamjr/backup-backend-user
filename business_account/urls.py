21d9c464c7a0797eca4f5446558eba3923ad49b3
ioiiiioi <sosmed.jefrihutama@gmail.com>
2019-02-25 23:57:22 +0700

    getname

--- business_account/urls.py
+++ business_account/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     url(r'^api/comp_bio/(?P<pk>[0-9]+)$', views.count_emp, name='count_emp'),
     url(r'^api/filter/(?P<pk>[0-9]+)$', views.get_all_businessaccount, name='get_all_businessaccount'),
     url(r'^api/search/$', views.search_company, name = 'search_company'),
-    url(r'^api/custom/apply/(?P<pk>[0-9]+)$$', views.custom_get_one, name='custom_get_one'),
+    url(r'^api/custom/apply/(?P<pk>[0-9]+)$', views.custom_get_one, name='custom_get_one'),
     url(r'^api/findcuremployee/(?P<pk>[0-9]+)$', views.cakarsebek, name='cakarsebek'),
     url(r'^api/get_name/(?P<pk>[0-9]+)$', views.get_name, name='get_name'),
-    url(r'^api/custom/active/(?P<pk>[0-9]+)$$', views.custom_get_two, name='custom_get_two')
+    url(r'^api/custom/active/(?P<pk>[0-9]+)$', views.custom_get_two, name='custom_get_two')
 ]
