1bc4039a62ee9f7d6432cf95d5cdde3233efd1aa
DaVinci <jefrihutamaarbi@gmail.com>
2019-04-29 13:13:02 +0700

    approval

--- approval_config/urls.py
+++ approval_config/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^api/$', views.migrate_to_approval, name='migrate_to_approval'),
-    url(r'^api/all/(?P<pk>[0-9]+)$', views.update_lookone_approval, name='update_lookone_approval'),
+    url(r'^api/(?P<pk>[0-9]+)$', views.update_lookone_approval, name='update_lookone_approval'),
     # url(r'^api/user/(?P<pk>[0-9]+)$', views.get_post_certification_user, name='get_post_certification_user'),
 ]
