40a9c254c6175ae918d68555897d14559ad3c0e8
Jefri Hutama Abri <jefrihutamaarbi@gmail.com>
2019-05-02 11:35:58 +0000

    Update views.py

--- vendor_api/views.py
+++ vendor_api/views.py
@@ -646,24 +646,24 @@ def cloning_data_reprime(request):
 			result = []
 			for id_hirar in hirarki:
 				hier = Hierarchy.objects.get(id = id_hirar)
-				# user = Register.objects.get(id  = hier.id_user)
+				user = Register.objects.get(id  = hier.id_user)
 				license = LicenseComp.objects.get(id_hierarchy = hier.id)				
-				# if license.attendance == '2':
-				# 	level = 'IsAdmin'
-				# elif license.attendance == '1':
-				# 	level = 'IsUser'
-				# else:
-				# 	level = 'User / Company Belum Mengaktifkan Fitur Ini'
-				# payload = {
-				# 'id' : user.id,
-				# 'fullname' : user.full_name,
-				# 'photo' : user.url_photo,
-				# 'level':level
-				# }
-				# result.append(payload)
+				if license.attendance == '2':
+					level = 'IsAdmin'
+				elif license.attendance == '1':
+					level = 'IsUser'
+				else:
+					level = 'User / Company Belum Mengaktifkan Fitur Ini'
+				payload = {
+				'id' : user.id,
+				'fullname' : user.full_name,
+				'photo' : user.url_photo,
+				'level':level
+				}
+				result.append(payload)
                 #----------------asdasd-------------
-				result.append(license.id)
-			return Response({'status':result}, status = status.HTTP_200_OK)
+# 				result.append(license.id)
+# 			return Response({'status':result}, status = status.HTTP_200_OK)
 			payloads = {
 			'company_id': company.id,
 			'company_name':company.company_name,
