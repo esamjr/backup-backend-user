2677a6e67c9446c7072406ff83a25f163fda74da
Jefri Hutama Abri <jefrihutamaarbi@gmail.com>
2019-05-02 13:09:53 +0000

    Update views.py

--- vendor_api/views.py
+++ vendor_api/views.py
@@ -642,12 +642,12 @@ def cloning_data_reprime(request):
 			vendor = Vendor_api.objects.get(token = token)
 			id_company = request.data['id_company']
 			company = Business.objects.get(id = id_company)
-			hirarki = Hierarchy.objects.all().values_list('id', flat = True).filter(id_company = company.id, status = '1')
+			hirarki = Hierarchy.objects.all().values_list('id', flat = True).filter(id_company = company.id)
 			result = []
 			for id_hirar in hirarki:
 				hier = Hierarchy.objects.get(id = id_hirar)
 				user = Register.objects.get(id  = hier.id_user)
-				license = LicenseComp.objects.get(id_hierarchy = hier.id)				
+				license = LicenseComp.objects.get(id_hierarchy = hier.id, status = '1')				
 				if license.attendance == '2':
 					level = 'IsAdmin'
 				elif license.attendance == '1':
