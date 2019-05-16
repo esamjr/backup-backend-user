0458fdd5a0bacf50519263d5df7beecd554bd0ac
DaVinci <jefrihutamaarbi@gmail.com>
2019-05-16 18:30:33 +0700

    sipromo

--- sipromo_api/views.py
+++ sipromo_api/views.py
@@ -9,72 +9,53 @@ import json
 
 @api_view(['POST'])
 def sipromo(request, stri):
-	if stri =='get_vouchers':
-		url = 'https://api.sipromo.id/get/vouchers'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken':'$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID':'92771509'
-		}
-		Req = requests.post(url, headers= header, data = payload)
-		Res = Res.json()
-	elif stri == 'get_voucher_detail':
-		url = 'https://api.sipromo.id/get/voucher_detail'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken': '$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID': '92771509',
-		'voucher_id': request.data['voucher_id']
-		}
-		Req = requests.post(url, headers = header, data = payload)
-		Res = Req.json()
-	elif stri == 'redeem_voucher':
-		token = request.META.get('HTTP_AUTHORIZATION')
-		user = Register.objects.get(token = token)
-		url = 'https://api.sipromo.id/redeem'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken': '$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID': '92771509',
-		'voucher_id': request.data['voucher_id'],
-		'user_id': user.id
-		}
-		Req = requests.post(url, headers = header, data = payload)
-		Res = Req.json()
-	elif stri == 'get_user_voucher_redeem':
-		token = request.META.get('HTTP_AUTHORIZATION')
-		user = Register.objects.get(token = token)
-		url = 'https://api.sipromo.id/get/user_voucher/redeem'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken': '$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID': '92771509',
-		'user_id': user.id
-		}
-		Req = requests.post(url, headers = header, data = payload)
-		Res = Req.json()
-	elif stri == 'get_user_voucher_used':
-		token = request.META.get('HTTP_AUTHORIZATION')
-		user = Register.objects.get(token = token)
-		url = 'https://api.sipromo.id/get/user_voucher/used'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken': '$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID': '92771509',
-		'user_id': user.id
-		}
-		Req = requests.post(url, headers = header, data = payload)
-		Res = Req.json()
-	elif stri == 'get_user_voucher_expired':
-		token = request.META.get('HTTP_AUTHORIZATION')
-		user = Register.objects.get(token = token)
-		url = 'https://api.sipromo.id/get/user_voucher/used'
-		header = {'Content-Type':'application/json'}
-		payload = {
-		'appToken': '$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6',
-		'appID': '92771509',
-		'user_id': user.id
-		}
-		Req = requests.post(url, headers = header, data = payload)
-		Res = Req.json()
-	return Response(Res)
+	try:
+		if stri =='getvouchers':
+			url = 'https://api.sipromo.id/get/vouchers'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\"\n}"
+			Req = requests.post(url, headers= header, data = payload)
+			Res = Req.json()
+		elif stri == 'getvoucherdetail':
+			url = 'https://api.sipromo.id/get/voucher_detail'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\",\n\t\"voucher_id\": \""+request.data['voucher_id']+"\"\n}"
+			Req = requests.post(url, headers = header, data = payload)
+			Res = Req.json()
+		elif stri == 'redeemvoucher':
+			token = request.META.get('HTTP_AUTHORIZATION')
+			user = Register.objects.get(token = token)
+			url = 'https://api.sipromo.id/redeem'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\",\n\t\"voucher_id\": \""+request.data['voucher_id']+"\",\n\t\"user_id\": \""+user.id+"\"\n}"
+			Req = requests.post(url, headers = header, data = payload)
+			Res = Req.json()
+		elif stri == 'getuservoucherredeem':
+			token = request.META.get('HTTP_AUTHORIZATION')
+			user = Register.objects.get(token = token)
+			url = 'https://api.sipromo.id/get/user_voucher/redeem'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\",\n\t\"user_id\": \""+user.id+"\"\n}"
+			Req = requests.post(url, headers = header, data = payload)
+			Res = Req.json()
+		elif stri == 'getuservoucherused':
+			token = request.META.get('HTTP_AUTHORIZATION')
+			user = Register.objects.get(token = token)
+			url = 'https://api.sipromo.id/get/user_voucher/used'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\",\n\t\"user_id\": \""+user.id+"\"\n}"
+			Req = requests.post(url, headers = header, data = payload)
+			Res = Req.json()
+		elif stri == 'getuservoucherexpired':
+			token = request.META.get('HTTP_AUTHORIZATION')
+			user = Register.objects.get(token = token)
+			url = 'https://api.sipromo.id/get/user_voucher/used'
+			header = {'Content-Type':'application/json'}
+			payload = "{\n\t\"appToken\": \"$2y$10$KMWlIAhy6p8gGPJx7tdgKOOgAIksjaaB6ZLUXWYtKxJH.9m/9oTy6\",\n\t\"appID\": \"92771509\",\n\t\"user_id\": \""+user.id+"\"\n}"
+			Req = requests.post(url, headers = header, data = payload)
+			Res = Req.json()
+		else:
+			return Response({'status':'Endpoint has no match'}, status = status.HTTP_400_BAD_REQUEST)
+		return Response(Res)
+	except Register.DoesNotExist:
+		return Response({'status':'User Did Not Exist'}, status = status.HTTP_401_UNAUTHORIZED)
