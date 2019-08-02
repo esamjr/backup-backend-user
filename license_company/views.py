ed1b5f4a81c8107b1920b340111adf381b26e20c
maulidan yulianto <maulidan@mindzzle.com>
2019-08-02 08:35:47 +0000

    Revert "tax num"
    This reverts commit 19d48e677187591b97760cec02347321f840151a

--- license_company/views.py
+++ license_company/views.py
@@ -81,8 +81,7 @@ def setting_license_company(request,pk):
                     user = Register.objects.get(id = dive.id_user)
                     persona = {
                     'id_user' : user.id,
-                    'name' : user.full_name,
-                    'tax':user.tax_num
+                    'name' : user.full_name
                     }
                     payload = {
                     'division': dive.division ,
