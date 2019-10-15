0ecfb56c976338f31dd15b14841d7edb46945dae
asepherb <asep.herbasuki@gramedia.digital>
2019-10-15 23:14:57 +0700

    * add CORS_ALLOW_HEADERS

--- backend_user/settings.py
+++ backend_user/settings.py
@@ -261,21 +261,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ORIGIN_WHITELIST = (
-    'http://dev-user.mindzzle.com',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
+#
+# CORS_ORIGIN_WHITELIST = (
+#     'http://dev-user.mindzzle.com',
+# )
+#
+# CORS_ALLOW_HEADERS = (
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+# )
