e021ccbb92333cbf1f6590d98e2d3537fe71a2b1
Asep Herbasuki <asp.shinichi@gmail.com>
2019-10-19 05:04:43 +0700

    * delete message

--- backend_user/urls.py
+++ backend_user/urls.py
@@ -69,7 +69,6 @@ urlpatterns = [
     url(r'^region/', include('region.urls')),
     url(r'^city/', include('city.urls')),
     url(r'^vendor/', include('vendor_api.urls')),
-    # url(r'^message/', include('message.urls')),
     url(r'^experiences/', include('experiences.urls')),
     url(r'^education/', include('education.urls')),
     url(r'^award/', include('award.urls')),
