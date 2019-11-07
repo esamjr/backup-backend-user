2048ed6fc8051dea21ea0866c97dedabadf8883f
Asep Herbasuki <asp.shinichi@gmail.com>
2019-11-07 14:24:48 +0700

    * rollback

--- registrations/urls.py
+++ registrations/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^apisuperadmin/$', views.auto_migrate_to_domoo, name='auto_migrate_to_domoo'),
     url(r'^api/(?P<pk>[0-9]+)$', views.get_delete_update_registrations, name='get_delete_update_registrations'),
-    url(r'^api/findFriends/(?P<pk>[0-9]+)$', views.get_user_by_email, name='get_user_by_email'),
+    url(r'^api/findfriends/(?P<pk>[0-9]+)$', views.get_user, name='get_user'),
     url(r'^api/$', views.get_post_registrations, name='get_post_registrations'),
     url(r'^api/upload/$', views.upload_xls, name='upload_xls'),
     url(r'^api/login/$', views.get_login, name='get_login'),
@@ -13,5 +13,4 @@ urlpatterns = [
     url(r'^api/forget/$', views.forget, name='forget'),
     url(r'^api/forgetbacklink/$', views.forget_backlink, name='forget_backlink'),
     url(r'^api/tes/$', views.attempt_login, name='attempt_login'),
-    url(r'^api/searchEmail/$', views.get_search_by_name, name='get_search_by_name'),
 ]
