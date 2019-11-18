20ced457d728704fc822368787c6c7a8f2145d80
Asep Herbasuki <asp.shinichi@gmail.com>
2019-11-18 08:48:28 +0700

    * fix error url

--- email_app/urls.py
+++ email_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^api/resendemail/$', views.resend_email, name='resend_email'),
+    url(r'^api/resendemail/$', views.resend_email, name='resend_email'),
     url(r'^api/sendmail/$', views.send_email, name='send_email'),
     url(r'^api/$', views.email_get, name='email_get'),
     url(r'^testing/$', views.percobaan, name='percobaan'),
