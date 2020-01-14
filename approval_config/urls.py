cb3d28fcf1303717ba6667680777bf145eda16a0
asepherb <asp.shinichi@gmail.com>
2020-01-14 12:56:47 +0700

    * fixing approval_config

--- approval_config/urls.py
+++ approval_config/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^api/$', views.migrate_to_approval, name='migrate_to_approval'),
-    url(r'^api/all/(?P<pk>[0-9]+)$', views.get_all_approval_by_comp, name='get_all_approval_by_comp'),
+    # url(r'^api/all/(?P<pk>[0-9]+)$', views.get_all_approval_by_comp, name='get_all_approval_by_comp'),
+    url(r'^api/all/(?P<pk>[0-9]+)', views.get_all_approval_by_comp, name='get_all_approval_by_comp'),
     url(r'^api/(?P<pk>[0-9]+)$', views.update_lookone_approval, name='update_lookone_approval'),
     # url(r'^api/user/(?P<pk>[0-9]+)$', views.get_post_certification_user, name='get_post_certification_user'),
 ]
