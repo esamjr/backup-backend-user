7fb49f0d7f212613bc908618bf4b15678fab6e40
asepherb <asp.shinichi@gmail.com>
2020-01-29 07:35:26 +0700

    * comment tokenauth temporary

--- backend_user/settings.py
+++ backend_user/settings.py
@@ -204,14 +204,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Auth
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
-}
+#
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated', )
+# }
 
 
 # Email setting
