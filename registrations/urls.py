929457a848fd9a8087d538aac16ccd3cf501cb66
asepherb <asp.shinichi@gmail.com>
2020-02-14 01:56:43 +0700

    * change login to new token

--- registrations/urls.py
+++ registrations/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^api/findfriends/(?P<pk>[0-9]+)$', views.get_user, name='get_user'),
     url(r'^api/$', views.get_post_registrations, name='get_post_registrations'),
     url(r'^api/upload/$', views.upload_xls, name='upload_xls'),
-    url(r'^api/login/$', views.get_login, name='get_login'),
+    # url(r'^api/login/$', views.get_login, name='get_login'),
     url(r'^api/search/$', views.search, name='search'),
     # url(r'^api/confirm/$', views.verified_acc, name='verified_acc'),
     url(r'^api/forget/$', views.forget, name='forget'),
@@ -15,7 +15,7 @@ urlpatterns = [
 
     # new api for mindzzle
     url(r'^api/confirm/$', views.activate_email, name='activate_email'),
-    url(r'^api/cek_login/$', views.cek_login_views, name='cek_login_views'),
+    url(r'^api/login/$', views.cek_login_views, name='cek_login_views'),
 
     # api for crocodic
     url(r'^api/cek_token/$', views.login_token_views, name='login_token_views'),
