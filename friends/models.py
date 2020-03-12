58f0e0ddf879bd8dd494a86395640128f5d77329
lisianthus <lisianthus623@gmail.com>
2020-03-12 14:17:50 +0700

    delete necessary comments

--- friends/models.py
+++ friends/models.py
@@ -5,7 +5,6 @@ from registrations.models import Register
 from django.core.cache import cache
 from django.utils.translation import ugettext_lazy as _
 
-
 class Friends(models.Model):
     user_id = models.IntegerField()
     user_name = models.CharField(max_length=60)
@@ -15,15 +14,3 @@ class Friends(models.Model):
         Register, related_name='friend_request', blank=True)
     waiting_for_response = models.ManyToManyField(
         Register, related_name='waiting_for_response', blank=True)
-
-# class Follow(models.Model):
-#     user_id = models.IntegerField()
-#     user_name = models.CharField(max_length=60)
-#     follower = models.ManyToManyField(Register, related_name='follower', blank=True)
-#     following = models.ManyToManyField(Register, related_name='following', blank=True)
-
-    # def save(self, *args, **kwargs):
-    #     # Ensure users can't be friends with themselves
-    #     if self.follower == self.following:
-    #         raise JsonResponse("Users cannot follow themselves.")
-    #     super(Follow, self).save(*args, **kwargs)
