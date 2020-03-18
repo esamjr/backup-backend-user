d8d04a03e12c50d16af018ead35a3ed756037840
asepherb <asp.shinichi@gmail.com>
2020-03-18 14:18:57 +0700

    * fixing logic user_image for ktp

--- OCR_Reader/serializers.py
+++ OCR_Reader/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from user_img.models import User_img
 from business_img.models import Company_img
 
-class UserImgSerializer(serializers.ModelSerializer):
 
-	class Meta:
-		model=User_img
-		fields='__all__'
+class UserImgSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User_img
+        fields='__all__'
+
 
 class CompImgSerializer(serializers.ModelSerializer):
     class Meta:
